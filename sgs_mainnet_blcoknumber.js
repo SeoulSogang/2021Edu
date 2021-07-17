d16dcc0c2b3efb3d4e5f15aa0bfada92f9937845
baselab1008 <53509223+baselab1008@users.noreply.github.com>
2021-07-17 20:40:01 +0900

    Delete sgs_mainnet_blcoknumber.js

--- sgs_mainnet_blcoknumber.js
+++
@@ -1,7 +0,0 @@
-let Web3 = require("web3");
-
-let web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/bf3e8b40dbbc43a5874fa764c866151e"));
-
-web3.eth.getBlockNumber((err, blockCount) => {
-    console.log(blockCount)
-});
